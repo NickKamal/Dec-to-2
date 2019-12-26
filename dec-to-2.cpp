7571ef66f2b17d8268fb4538f2111a8dc7ebbc71
Nick Kamal <nirsukhandeep@gmail.com>
2019-12-25 20:03:32 -0800

    Done

--- dec-to-2.cpp
+++ dec-to-2.cpp
@@ -8,8 +8,13 @@ int main()
 {
 
     int input = 0;
-    cout<<"Enter the number in decimal: ";
+    cout<<"Enter the positive number in decimal: ";
     cin>>input;
+    if(input < 0)
+    {
+        cout<<"Invalid input!!"<<endl;
+        return -1;
+    }
     if(input == 0)
     {
         cout<<"Binary value:: "<<0<<endl;
